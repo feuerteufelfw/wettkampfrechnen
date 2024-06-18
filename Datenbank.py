5ea18433da6e62b901f541746805db17ab92d550
feuerteufelfw <feuerteufel.fw@gmail.com>
2024-06-18 11:48:57 +0200

    Schön und mit Teilnehmer daten

--- Datenbank.py
+++ Datenbank.py
@@ -1,4 +1,6 @@
 import sqlite3
+import time
+
 
 class datenbank:
 
@@ -11,21 +13,12 @@ class datenbank:
         self.db.commit()
         self.db.close()
 
-    def get_disziplin_nr(self,disziplin):
-        print(disziplin)
-        sql_befehl =f"Select Disziplin_NR from Disziplin \
-        Where Disziplin = %s;"%(disziplin)
-        self.cursor.execute(sql_befehl)
-        disziplin_nr = self.cursor.fetchall()
-        print(disziplin_nr)
-        return disziplin_nr
-
     def ad_teilnehmer(self,teilnehmer):
+        # fügt einen neuen Teilnehmer in die DB hinzu
         disziplin_nr = self.get_disziplin_nr(teilnehmer["disziplin"])
         sql_befehl = "INSERT INTO Teilnehmer (Start_Nr,Vorname,Nachname,\
         Ak,Geburtsdatum,Verein,geschlecht,disziplin_nr,email,schule)\
         Values (?,?,?,?,?,?,?,?,?,?)"
-        print(teilnehmer)
         self.cursor.execute(sql_befehl,(teilnehmer["start_nr"],\
                                         teilnehmer["vorname"],\
                                         teilnehmer["name"],\
@@ -35,68 +28,46 @@ class datenbank:
                                         teilnehmer["geschlecht"],\
                                         disziplin_nr,\
                                         teilnehmer["email"],\
-                                        teilnehmer["schule"]))#auto increment wert zurückgeben
+                                        teilnehmer["schule"]))
         self.db.commit()
-        #bekomme höchste tn für disziplin, +1
 
-    def test_tabelle_vorhanden(self,name,datenbank) :
-        try:
-            db = sqlite3.connect(datenbank)
-            cursor = db.cursor()
-            sql_command ='''Exist SELECT * FROM sqlite_master\
-                            WHERE name="''' +name+ '''";'''
-            print(sql_command)
-            cursor.execute(sql_command)
-            temp = cursor.fetchone()
-            print(temp)
-            return True
-        except:
-            return False
-
-    def get_disziplin_nr(self,Name_disziplin): #gibt die der disziplin zugeordneten NR zurück
+    def get_disziplin_nr(self,Name_disziplin):
+        # gibt die der Disziplin zugeordneten Nummer zurück
         sql_command = "SELECT Disziplin_Nr From Disziplin \
                         Where Disziplin = '%s';" %(Name_disziplin)
-        print(sql_command)
         self.cursor.execute(sql_command)
         return self.cursor.fetchone()[0]
 
-    def insert_new_disziplin(self,Disziplin,Urkunde,): #erstellt neuen db Eintrag disziplin, Disziplin ist der namen und Urkunde der Urkundenfile
+    def insert_new_disziplin(self,Disziplin,Urkunde,):
+        # erstellt neuen db Eintrag Disziplin
         sql_command = "INSERT INTO Disziplin (Urkunde,Disziplin) VALUES(?,?);"
         self.cursor.execute(sql_command, (Urkunde, Disziplin))
         self.db.commit()
 
-    def get_start_nr(self,tn,disp_nr): #gibt die Startnummer des TN für die Disziplin zurück
-        sql_command =" SELECT Start_Nr FROM schwimmt \
-                        WHERE Tn = '%s' & Disziplin_Nr = '%s';" %(tn,disp_nr)
-        self.cursor.execute(sql_command)
-        return self.cursor.fetchall()
-
-    def erstelle_tabelle(self,tabel_name,spalten):
-        sql_command ="Create Table " + tabel_name + "(" + spalten +");"
-        self.cursor.execute(sql_command)
-        self.db.commit()
-
     def insert_into_zeiten(self,zeit,diziplin_nr):
+        # fügt eine neue Zeit hinzu
         sql_command = "INSERT INTO zeiten (zeit,disziplin_nr) VALUES(?,?);"
         self.cursor.execute(sql_command,(zeit,diziplin_nr))
         self.db.commit()
 
     def ad_tn_to_zeiten(self,start_nr,zeit):
+        # Fügt einer Zeit eine Startnummer hinzu
         sql_command ="Update Zeiten Set Start_Nr = '%s'\
-                        Where Zeiten = %s;"%(int(start_nr),zeit)
-        print(sql_command)
+                        Where Zeiten = '%s';"%(int(start_nr),zeit)
         self.cursor.execute(sql_command)
         self.db.commit()
+        time.sleep((0.2))
 
     def get_zeiten(self,disziplin_nr):
-        print(disziplin_nr)
+        # Gibt die gemessenen Zeiten zu einer Disziplin zurück
         sql_command = "SELECT * FROM zeiten \
                         WHERE disziplin_nr ='%s';" %(disziplin_nr)
-        print(sql_command)
         self.cursor.execute(sql_command)
         return self.cursor.fetchall()
 
     def get_sortet_time(self,disziplin_nr,ak,geschlecht):
+        # gibt zeiten und Startnummern einer Disziplin
+        # sortiert zurück, in Bezug auf Altersklasse und Geschlecht
         sql_command = "Select z.Zeiten,t.Start_Nr \
                         From Teilnehmer As t,Zeiten As z \
                         Where t.Start_Nr = z.Start_Nr \
@@ -104,11 +75,13 @@ class datenbank:
                          AND t.Ak = '%s' \
                          And t.geschlecht = '%s'  \
                          ORDER BY z.Zeiten;"%(disziplin_nr,ak,geschlecht)
-        print(sql_command)
+
         self.cursor.execute(sql_command)
         return self.cursor.fetchall()
 
     def get_sortet_time_ges(self,disziplin,geschlecht ):
+        # gibt zeiten und Startnummern einer Disziplin
+        # sortiert zurück, unter Beachtung des Geschlechtes
         sql_command ="Select z.Zeiten,t.Start_Nr \
                         From Teilnehmer AS t, Zeiten As z \
                             Where t.Start_Nr =z.Start_Nr \
@@ -119,63 +92,59 @@ class datenbank:
         return self.cursor.fetchall()
 
     def get_tn_infos(self,start_nr):
+        # gibt hinterlegte Infos zu einer Startnummer zurück
         sql_command = "Select * From Teilnehmer \
                         Where Start_Nr = '%s';" %(start_nr)
         self.cursor.execute(sql_command)
         return self.cursor.fetchall()
 
-    def insert_zeiten(self,zeit,start_nr,pos):
-        sql_command = "Update Teilnehmer Set Zeit = '%s', pos_ges = '%s'\
-                        WHERE Start_Nr = '%s' ;"%(zeit,pos,start_nr)
-        self.cursor.execute(sql_command)
-        self.db.commit()
-
     def get_aks(self,disziplin_nr):
+        # gibt Altersklassen, welche die Disziplin schwimmen zurück
         sql_command ="Select aks From Disziplin \
                        WHERE Disziplin_Nr = '%s';"%(disziplin_nr)
         self.cursor.execute(sql_command)
         return self.cursor.fetchall()
 
-    def add_pos(self,pos,start_nr):
-        sql_command ="Update Teilnehmer Set pos = '%s' \
-                        WHERE Start_Nr = '%s'; " %(pos,start_nr)
-        self.cursor.execute(sql_command)
-        self.db.commit()
-
     def get_Teilnehmer(self):
+        # gibt alle gespeicherten Teilnehmer zurück
         sql_command =" SELECT * From Teilnehmer; "
         self.cursor.execute(sql_command)
         teilnehmer_list = self.cursor.fetchall()
         return teilnehmer_list
 
     def get_disziplin(self,disziplin_nr):
+        # gibt Namen der Disziplin zurück
         sql_command ="Select Disziplin from Disziplin\
                         Where Disziplin_Nr = '%s';" %(disziplin_nr)
         self.cursor.execute(sql_command)
         return self.cursor.fetchall()
 
     def insert_pos_ergebnis(self,pos,start_nr):
+        # fügt eine Startnummer und deren Position
+        # in die Tabelle ergebnisse hinzu
         sql_command ="  Insert Into ergebnisse (Pos,Start_Nr)\
                         SELECT '%s', '%s' WHERE NOT EXISTS\
                         (SELECT * From ergebnisse \
                             WHERE Start_Nr = '%s') ;"%(pos,start_nr,start_nr)
-        print(sql_command)
         self.cursor.execute(sql_command)
         self.db.commit()
 
     def get_pos(self,start_nr):
+        # Gibt die Position und die Gesamtposition einer Startnummer zurück
         sql_command = "SELECT Pos, Pos_Gesamt From ergebnisse \
                         Where Start_Nr = '%s';"%(start_nr)
         self.cursor.execute(sql_command)
         return self.cursor.fetchone()
 
     def get_time(self,start_nr):
+        # Gibt Zeit einer Startnummer zurück
         sql_command = "SELECT Zeiten From Zeiten \
                         Where Start_Nr = '%s';"%(start_nr)
         self.cursor.execute(sql_command)
         return self.cursor.fetchone()
 
     def update_tn(self,start_nr,row):
+        # Updatet die zu einer Startnummer hinterlegten Teilnehmerdaten
         vorname =row.get("vorname")
         name = row.get("name")
         verein = row.get("verein")
@@ -198,25 +167,29 @@ class datenbank:
         self.db.commit()
 
     def insert_zeit(self,zeit,disziplin_nr):
-        print(zeit)
-        print(disziplin_nr)
+        # speichert eine Zeit
+        # und die Diszplinnummer auf welche sich die Zeit bezieht
         sql_command ="Insert INto Zeiten (Zeiten,disziplin_nr) Values(?,?);"
         self.cursor.execute(sql_command,(zeit,disziplin_nr))
         self.db.commit()
 
     def insert_time(self,start_nr,zeit,disziplin_nr):
+        # speichert eine Zeit, die entsprechende Disziplin Nummer
+        # und die zugeordnete Startnummer
         sql_command ="Insert Into Zeiten (Start_Nr,Zeiten,disziplin_nr)\
                         Values(?,?,?);"
         self.cursor.execute(sql_command,(start_nr,zeit,disziplin_nr))
         self.db.commit()
 
     def insert_pos_gesamt(self,start_nr,pos_ges):
+        # fügt Gesamtpostion hinzu
         sql_command = "Update ergebnisse Set Pos_Gesamt = '%s'\
                         Where Start_Nr = '%s';" %(pos_ges,start_nr)
         self.cursor.execute(sql_command)
         self.db.commit()
 
     def get_ergebnisse_disziplin(self,disziplin_nr):
+        # gibt alle Ergebnisse einer Disziplin zurück
         sql_command = ("Select e.Start_Nr,t.Vorname,t.Nachname,\
                         z.Zeiten,Ak,e.Pos\
                         From Teilnehmer AS t, Zeiten AS z, ergebnisse AS e \
@@ -227,6 +200,7 @@ class datenbank:
         return self.cursor.fetchall()
 
     def get_ergebnisse(self,disziplin_nr,geschlecht):
+        # gibt die Ergebnisse eines Geschlechtes in einer Disziplin zurück
         sql_command = ("Select e.Pos_Gesamt,z.Zeiten,t.Vorname,t.Nachname,\
                         t.geschlecht,Ak,e.Pos,t.Verein \
                         From Teilnehmer AS t, Zeiten AS z, ergebnisse AS e \
